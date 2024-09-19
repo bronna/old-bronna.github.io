e341db50e5aaffdfc18dda7272dba8259d055356
Brianna Wilson <bronna@gmail.com>
2024-09-19 16:04:31 -0700

    config changes

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,11 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		})
 	}
 };
 
